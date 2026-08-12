919ea4a42a34299384864e97edfd12e7743665cf
tinyu070226-stack <tinyu070226@gmail.com>
2026-08-12 11:48:38 +0800

    fix_sw_syntax

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-self.addEventListener(" install\, () => self.skipWaiting()); self.addEventListener(\activate\, event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))).then(() => self.clients.claim()); });
+// Service Worker - No Cache (always fetch fresh)
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+        .then(() => self.clients.claim())
+    );
+});
+self.addEventListener('fetch', event => {
+    event.respondWith(fetch(event.request));
+});
